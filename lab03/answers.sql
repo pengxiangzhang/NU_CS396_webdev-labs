5a6e85f4df120b4ed95df38e677c72c5061242d9
Pengxiang Zhang <z-git@protonmail.com>
2022-01-19 14:14:05 -0600

    Commiting my completed lab03 files

--- lab03/answers.sql
+++ lab03/answers.sql
@@ -29,38 +29,42 @@ SELECT COUNT(*)  FROM posts;
 
 
 -- Exercise 7
-
+SELECT user_id, COUNT(id) as count FROM comments GROUP BY user_id ORDER BY count DESC;
 
 
 
 -- Exercise 8
-
+SELECT posts.id, posts.image_url, posts.user_id, users.username, users.first_name, users.last_name user_id FROM posts INNER JOIN users ON posts.user_id = users.id WHERE posts.user_id=26 OR posts.user_id=12;
 
 
 
 -- Exercise 9
-
+SELECT posts.id, posts.pub_date, following.following_id FROM posts INNER JOIN following ON posts.user_id = following.following_id WHERE following.user_id=26;
 
 
 
 -- Exercise 10
-
+    -- Optional
 
 
 
 -- Exercise 11
-
+INSERT INTO bookmarks (user_id,post_id) VALUES (26, 219);
+INSERT INTO bookmarks (user_id,post_id) VALUES (26, 220);
+INSERT INTO bookmarks (user_id,post_id) VALUES (26, 221);
 
 
 
 -- Exercise 12
-
-
+DELETE FROM bookmarks WHERE user_id=26 AND post_id=219;
+DELETE FROM bookmarks WHERE user_id=26 AND post_id=220;
+DELETE FROM bookmarks WHERE user_id=26 AND post_id=221;
 
 
 -- Exercise 13
-
+UPDATE users set email='cyoung2022@gmail.com' WHERE id=26;
 
 
 
 -- Exercise 14
+    -- Optional
