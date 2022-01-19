dbfd7282c181313f2d486ec300238e5a28890d47
Pengxiang Zhang <z-git@protonmail.com>
2022-01-19 02:00:43 -0600

    Initial Lab3

---
+++ lab03/answers.sql
@@ -0,0 +1,66 @@
+-- Exercise 1 (done for you):
+SELECT * FROM users;
+
+
+
+-- Exercise 2 (done for you):
+SELECT id, first_name, last_name FROM users;
+
+
+
+-- Exercise 3
+SELECT id, first_name, last_name FROM users ORDER BY last_name;
+
+
+
+-- Exercise 4
+SELECT id, image_url, user_id FROM posts WHERE user_id=26;
+
+
+
+-- Exercise 5
+SELECT id, image_url, user_id FROM posts WHERE user_id=26 OR user_id=12;
+
+
+
+-- Exercise 6
+SELECT COUNT(*)  FROM posts;
+
+
+
+-- Exercise 7
+
+
+
+
+-- Exercise 8
+
+
+
+
+-- Exercise 9
+
+
+
+
+-- Exercise 10
+
+
+
+
+-- Exercise 11
+
+
+
+
+-- Exercise 12
+
+
+
+
+-- Exercise 13
+
+
+
+
+-- Exercise 14
