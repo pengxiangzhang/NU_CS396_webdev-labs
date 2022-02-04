41f2d61127ad4835a423b3a8dc82aa458f161130
Pengxiang Zhang <z-git@protonmail.com>
2022-02-03 22:35:49 -0600

    Update Base on Lab Session

--- lab05/01-font-switcher/index.js
+++ lab05/01-font-switcher/index.js
@@ -1,28 +1,19 @@
+let currentFont = 1.4;
+
 const makeBigger = () => {
-  var h1 = document.querySelector("h1");
-  var styleH1 = window.getComputedStyle(h1, null).getPropertyValue("font-size");
-  var currentSizeH1 = parseInt(styleH1);
-  currentSizeH1 += 5;
-  h1.style.fontSize = currentSizeH1 + "px";
-  var p = document.querySelector("p");
-  var styleP = window.getComputedStyle(p, null).getPropertyValue("font-size");
-  var currentSizeP = parseInt(styleP);
-  currentSizeP += 5;
-  p.style.fontSize = currentSizeP + "px";
+currentFont+=0.2
+setFont();
 };
 
 const makeSmaller = () => {
-  var h1 = document.querySelector("h1");
-  var styleH1 = window.getComputedStyle(h1, null).getPropertyValue("font-size");
-  var currentSizeH1 = parseInt(styleH1);
-  currentSizeH1 -= 5;
-  h1.style.fontSize = currentSizeH1 + "px";
-  var p = document.querySelector("p");
-  var styleP = window.getComputedStyle(p, null).getPropertyValue("font-size");
-  var currentSizeP = parseInt(styleP);
-  currentSizeP -= 5;
-  p.style.fontSize = currentSizeP + "px";
+  currentFont-=0.2
+  setFont();
 };
+
+const setFont =() =>{
+  document.querySelector('h1').style.fontSize = `${currentFont}em`;
+  document.querySelector('.content').style.fontSize = `${currentFont}em`;
+}
 
 document.getElementById("a1").addEventListener("click", makeBigger);
 document.getElementById("a2").addEventListener("click", makeSmaller);
